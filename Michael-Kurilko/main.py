fb86eb580dbfa9d441e25c5d146da6920fe5923f
Michael Kurilko <michael.kurilko7@gmail.com>
2025-11-09 11:39:46 -0500

    90

--- Michael-Kurilko/main.py
+++ Michael-Kurilko/main.py
@@ -1,2 +1,1 @@
-a,b=0,1
-for i in range(47):print(a)if a<2or any(a%j<1for j in range(2,int(a**.5)+1))else None;a,b=b,a+b
+a,b=0,1;exec("(a<2or any(a%j<1for j in range(2,int(a**.5+1))))and print(a);a,b=b,a+b;"*47)
